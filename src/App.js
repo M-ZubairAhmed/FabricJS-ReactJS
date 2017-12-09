177d4fb89f24572aa6362a47b728c00e4153a9b7
z <ahmedzubair216@gmail.com>
2017-12-09 17:32:21 +0530

    demarkation of customization and creation

--- src/App.js
+++ src/App.js
@@ -14,31 +14,49 @@ class App extends Component {
     }
   }
 
+  renderCustomizationProduct() {
+    return (
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <Canva {...this.state} />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <div style={{ border: '1px solid #000', margin: 10, padding: 10 }}>
+            <div style={{ textAlign: 'center', fontWeight: 'bold' }}>
+              Customization Tools
+            </div>
+            <div style={{ display: 'flex' }}>
+              <div style={{ marginRight: 10 }}>Add customized image URL:</div>
+              <input
+                size="40"
+                placeholder="Enter image URL"
+                onChange={e =>
+                  this.setState({ customImageURL: e.target.value })
+                }
+              />
+            </div>
+          </div>
+        </Grid.Column>
+      </Grid.Row>
+    )
+  }
+
+  renderCreatingProduct() {
+    return (
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <div>here is canvas</div>
+        </Grid.Column>
+      </Grid.Row>
+    )
+  }
+
   render() {
     return (
       <Grid padded>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Canva {...this.state} />
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <div style={{ border: '1px solid #000', margin: 10, padding: 10 }}>
-              <div style={{ textAlign: 'center', fontWeight: 'bold' }}>
-                Customization Tools
-              </div>
-              <div style={{ display: 'flex' }}>
-                <div style={{ marginRight: 10 }}>Add customized image URL:</div>
-                <input
-                  size="40"
-                  placeholder="Enter image URL"
-                  onChange={e =>
-                    this.setState({ customImageURL: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-          </Grid.Column>
-        </Grid.Row>
+        {this.state.productType === 'trophy'
+          ? this.renderCreatingProduct()
+          : this.renderCustomizationProduct()}
       </Grid>
     )
   }
